c51647cb77d30fe3f17f06106bfb6bda59e59d61
VERIFIEDB0T <alon.feldman10@gmail.com>
2026-05-20 19:05:18 +0300

    server ui first steps

---
+++ Server/ServerWindow.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Server
+{
+    partial class ServerWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(150, 100);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(100, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(180, 65);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(35, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Port";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(25, 71);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(100, 30);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(25, 129);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(100, 30);
+            button2.TabIndex = 3;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(280, 100);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(104, 20);
+            label2.TabIndex = 4;
+            label2.Text = "IP placeholder";
+            // 
+            // ServerWindow
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(982, 553);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "ServerWindow";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
