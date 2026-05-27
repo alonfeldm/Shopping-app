78d4f5e77383384d089275b2ce963efdda45559b
VERIFIEDB0T <alon.feldman10@gmail.com>
2026-05-28 00:19:53 +0300

    now connects

--- Server/obj/Debug/net10.0-windows/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0-windows/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82200e13629ecfc70b4028019d4a29016e23752e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1084581574a426b9bb7bc2d794c5dbf43b54cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
