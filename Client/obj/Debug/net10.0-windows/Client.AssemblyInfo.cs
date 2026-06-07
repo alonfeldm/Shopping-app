4cf56ec6280a27a39765c7f163d4569d2c2e2e00
VERIFIEDB0T <alon.feldman10@gmail.com>
2026-06-07 17:05:48 +0300

    finishing touches

--- Client/obj/Debug/net10.0-windows/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net10.0-windows/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a716342ad32c90e4d9a2cb268c5e52f3951cc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc8dd5906822e9a6a1df8d7c433cae9d76b1cb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
