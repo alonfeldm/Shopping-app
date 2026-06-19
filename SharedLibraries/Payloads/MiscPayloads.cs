58783ba1e35bf5a11bf3ddae645f95efe66163db
VERIFIEDB0T <alon.feldman10@gmail.com>
2026-06-19 19:06:14 +0300

    cleaned

--- SharedLibraries/Payloads/MiscPayloads.cs
+++
@@ -1,6 +0,0 @@
-namespace SharedLibraries.Payloads;
-
-public sealed partial class ErrorPayload
-{
-    public string ErrorMessage { get; set; } = string.Empty;// hold the error message to be logged
-}
